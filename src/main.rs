5f5ab0c0870f5b7d2bd470046145003553ebb21a
otto-dae <otto.acha.28@gmail.com>
2025-09-01 14:32:45 -0600

    learned how to nest urls

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use axum::{
-    response::{Html}, routing::{get}, Router
+    http::StatusCode, response::Html, routing::get, Router
 };
 
 #[tokio::main]
 async fn main(){
-    let app = Router::new().route("/", get(handler_hello));
+    
+    let vehicle_routes = Router::new()
+    .route("/", get(handler_vehicles));
+
+    let owner_routes = Router::new()
+    .route("/", get(handler_owners));
+    
+    let api_routes = Router::new()
+    .nest("/vehicles", vehicle_routes)
+    .nest("/owners", owner_routes)
+    .fallback(fallback);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
 
-    axum::serve(listener, app).await.unwrap()
+    axum::serve(listener, api_routes).await.unwrap()
 }
 
-async fn handler_hello() -> Html<&'static str>{
-    println!("->> {:<12} - handler_hello", "HANDLER");
+async fn handler_vehicles() -> Html<&'static str>{
+        println!("->> {:<12} - handler_vehicles", "HANDLER");
+        Html("<h1>Hello, This is vehicles</h1>")
+}
 
-    Html("<h1>Hello, Axum!</h1><p>This is some HTML content.</p>")
+async fn handler_owners() -> Html<&'static str>{
+        println!("->> {:<12} - handler_vehicles", "HANDLER");
+        Html("<h1>Hello, This is owners</h1>")
+}
+
+async fn fallback() -> (StatusCode, &'static str){
+    (StatusCode::NOT_FOUND, "Not found")
 }
